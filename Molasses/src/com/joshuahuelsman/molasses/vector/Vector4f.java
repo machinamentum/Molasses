ed400ef44de2a1cc3473a03c5fb63cc3b83f1148
Joshua Huelsman <joshuahuelsman@gmail.com>
2013-05-20 10:36:30 -0400

    Added license statement to Vector and Vector4f.

--- Molasses/src/com/joshuahuelsman/molasses/vector/Vector4f.java
+++ Molasses/src/com/joshuahuelsman/molasses/vector/Vector4f.java
@@ -1,3 +1,8 @@
+/*
+ * This file was written by Joshua Huelsman. Do whatever you want
+ * with this stuff. If you think it's worth it, and we someday
+ * meet, you can buy me an energy drink.
+ */
 package com.joshuahuelsman.molasses.vector;
 
 public strictfp class Vector4f extends Vector {
